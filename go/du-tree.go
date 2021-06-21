503622e0170eed0c1aeb26f3aea896cc313ac125
Kostyn, Richard <richard.kostyn@sciencelogic.com>
2021-06-21 15:00:08 -0400

    Attempting to parse args

--- go/du-tree.go
+++ go/du-tree.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"flag"
+)
+
+func init(){
+	var help bool
+
+	flag.BoolVar( &help, "help", true, "Pass to print help message" )
+
+	flag.Parse()
+}
 
 func main(){
+	fmt.Println("help has value", help)
+
 	fmt.Println("Hello World")
-	
+
 }
